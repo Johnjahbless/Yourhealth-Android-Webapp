1197c62ecd2c8caab0e963a5e73306b984af8852
John jahbless <Johnjahbless@gmail.com>
2018-09-29 22:27:09 +0100

    addedBottomNavigation

--- app/src/main/java/com/junit/apps/yourhealth/MainActivity.java
+++ app/src/main/java/com/junit/apps/yourhealth/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,11 +19,36 @@ public class MainActivity extends AppCompatActivity {
     WebView webview;
     SwipeRefreshLayout swipeRefreshLayout;
     RelativeLayout layout;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_back:
+                    if (webview.canGoBack()) {
+                        webview.goBack();
+                    }
+                    return true;
+                case R.id.navigation_home:
+                    webview.loadUrl("https://yourhealth-d2683.firebaseapp.com");
+                    return true;
+                case R.id.navigation_forward:
+                    if (webview.canGoForward()) {
+                        webview.goForward();
+                    }
+                    return true;
+            }
+            return false;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
         webview = (WebView) findViewById(R.id.wv_test);
 
         layout = (RelativeLayout) findViewById(R.id.layout);
@@ -29,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                webview.loadUrl("https://yourhealth-d2683.firebaseapp.com");
+                webview.loadUrl("javascript:window.location.reload( true )");
 
             }
         });
@@ -86,7 +115,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.exit) {
+            finish();
+        }
+        return false;
+    }
         @Override
     public void onBackPressed() {
             if (webview.canGoBack()) {
